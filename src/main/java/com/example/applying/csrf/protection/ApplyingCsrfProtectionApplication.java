d547c41f6bc6be0cd3b4186d5d87c7fd5f0b75d5
thanhnd <ducthanhnguyen95@gmail.com>
2023-07-14 06:02:02 +0700

    Using CSRF protection in practical scenarios

---
+++ src/main/java/com/example/applying/csrf/protection/ApplyingCsrfProtectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.applying.csrf.protection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplyingCsrfProtectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplyingCsrfProtectionApplication.class, args);
+	}
+
+}
